d9731c0fd96191a7e6ad8eb89fd2072b2f1f4316
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-05-07 14:46:22 +0700

    install package notenv, node-schedule, pm2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+const schedule = require('node-schedule');
 const connectDatabase = require('./database/database');
 const crawlerProject = require('./services/project.crawler');
 const crawlerPost = require('./services/post.crawler');
 
-connectDatabase(() => {
-    // Queue just one URL, with default callback
+schedule.scheduleJob('17 * * * *', connectDatabase(() => {
+    // Queue just one URL, with default callbackadd
     crawlerPost();
     crawlerProject();
-});
+}));
+
--- database/database.js
+++ database/database.js
@@ -1,9 +1,7 @@
-const config = require('config');
-const mongoConfig = config.get('mongo');
 const mongoose = require('mongoose');
 
 module.exports = (callback) => {
-    mongoose.connect(mongoConfig.uri, {useNewUrlParser: true}, function(err){
+    mongoose.connect(process.env.mongo_uri, {useNewUrlParser: true}, function(err){
         if(err) {
             console.log(err);
             //throw err;
