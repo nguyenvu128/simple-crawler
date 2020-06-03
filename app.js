0db04a9df44fb025997b276805a25b85a2f41626
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-03 18:06:47 +0700

    create users model

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const findProject = require('./services/findAndUpdateProject');
 
 connectDatabase(() => {
     if(process.env.dev === 'true'){
-        crawlerPost();
+        // crawlerPost();
         // crawlerProject();
-        // findProject();
+        findProject();
     }else {
         schedule.scheduleJob(process.env.cron_jobs_start_at, function(){
             // Queue just one URL, with default callbackadd
